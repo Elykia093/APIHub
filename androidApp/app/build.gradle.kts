2e1281e116444a069b4559f3484fac450e04080c
Elykia <elykia@qq.com>
2026-07-23 13:51:14 +0800

    release: prepare v0.1.0-rc.2

--- androidApp/app/build.gradle.kts
+++ androidApp/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.elykia.apihub"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1.0-rc.1"
+        versionCode = 2
+        versionName = "0.1.0-rc.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
     }
