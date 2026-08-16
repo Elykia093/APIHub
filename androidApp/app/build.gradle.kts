b31b430ccc97c9febeca45360d058d9140f5362c
Elykia <elykia@qq.com>
2026-08-17 01:14:16 +0800

    fix: restore HTTP web UI in v0.1.0-rc.5

--- androidApp/app/build.gradle.kts
+++ androidApp/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.elykia.apihub"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "0.1.0-rc.4"
+        versionCode = 5
+        versionName = "0.1.0-rc.5"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
     }
